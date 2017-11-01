70b96d3333d2e51dff1346541d5de670f2ce84e6
Ravinsan Parameswaram <ravinsant20@gmail.com>
2017-11-01 15:00:13 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -41,6 +41,7 @@ describe('index', () => {
       expect(spy).toHaveBeenCalled('expected console.warn to have been called')
       console.warn.restore()
       done()
+      console.error("HALP!")
     })
   })
 })
